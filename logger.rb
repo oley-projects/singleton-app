8e5a572a02e9ccd980e8e49b4d668dfe7f370c77
Oley-Projects <oley.for.git@gmail.com>
2017-12-25 23:55:06 +0200

    Singleton ruby include

--- logger.rb
+++ logger.rb
@@ -1,17 +1,12 @@
+require 'singleton'
+
 class Logger
+  include Singleton
   def initialize
     @f = File.open 'log.txt', 'a'
-  end
-
-  @@x = Logger.new
-
-  def self.instance
-    return @@x
   end
 
   def log_something wat
     @f.puts wat
   end
-
-  private_class_method :new
 end
